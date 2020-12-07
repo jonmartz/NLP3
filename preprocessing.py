0a543a73b042475cab10ea5003fcbc28b542c81c
jonmartz <jonmartz91@gmail.com>
2020-12-07 17:03:29 +0200

    Original dataset preprocessing

--- preprocessing.py
+++ preprocessing.py
@@ -1,3 +1,15 @@
 import pandas as pd
+import csv
+from datetime import datetime
 
-trump_start_date = '2017-01-20'
+
+dateparse = lambda x: datetime.strptime(str(x), '%Y-%m-%d %H:%M:%S')
+
+cols = ['tweet id', 'user handle', 'tweet text', 'time stamp', 'device']
+dataset = pd.read_csv('trump_train.tsv', sep='\t', names=cols)
+dataset.dropna(inplace=True)
+dataset.drop(columns=['tweet id'], inplace=True)
+dataset['time stamp'] = dataset['time stamp'].apply(dateparse)
+dataset['label'] = (dataset['device'] == 'android') & (dataset['time stamp'] < datetime(2017, 4, 1))
+
+print('done')
