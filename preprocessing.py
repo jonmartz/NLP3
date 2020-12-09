110f675dea7144eafb403dcd522fa9994fadcba5
jonmartz <jonmartz91@gmail.com>
2020-12-09 19:15:13 +0200

    Implementing RNN

--- preprocessing.py
+++ preprocessing.py
@@ -1,15 +1,31 @@
 import pandas as pd
 import csv
 from datetime import datetime
+import nltk
 
-
+# data_subset = 'train'
+data_subset = 'test'
 dateparse = lambda x: datetime.strptime(str(x), '%Y-%m-%d %H:%M:%S')
 
 cols = ['tweet id', 'user handle', 'tweet text', 'time stamp', 'device']
-dataset = pd.read_csv('trump_train.tsv', sep='\t', names=cols)
+if data_subset == 'test':
+    cols = cols[1:-1]  # there's no label
+dataset = pd.read_csv('trump_%s.tsv' % data_subset, sep='\t', names=cols)
 dataset.dropna(inplace=True)
-dataset.drop(columns=['tweet id'], inplace=True)
+if data_subset == 'train':
+    dataset.drop(columns=['tweet id'], inplace=True)
 dataset['time stamp'] = dataset['time stamp'].apply(dateparse)
-dataset['label'] = (dataset['device'] == 'android') & (dataset['time stamp'] < datetime(2017, 4, 1))
+if data_subset == 'train':
+    label_col = (dataset['device'] == 'android') & (dataset['time stamp'] < datetime(2017, 4, 1))
+    dataset['label'] = label_col.astype(int)
+dataset.to_csv('dataset_%s.csv' % data_subset, index=False)
 
-print('done')
+
+def tokenize(text):
+    valid_tokens = []
+    tokens = nltk.word_tokenize(text)
+    for token in tokens:
+        token = token.lower()
+        if token.replace('.', '').isalpha():
+            valid_tokens.append(token)
+    return valid_tokens
